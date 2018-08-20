88741b0ddfaa724ba06cfa1b7a517d8f01ee0d27
wbhm <wbhmoore@gmail.com>
2018-08-20 10:45:28 -0500

    added sample images and data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/sampleData';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
+      const list = data;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Button variant="contained" color="primary">Sample Button </Button>
+          <div>
+              {
+                  list.map((item)=>{
+                      return <div key={item.id}>{item.name}</div>
+                  })
+              }
+          </div>
       </div>
     );
   }
