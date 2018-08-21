2658595c8e37602caf372d19137c148e92ce3a9b
wbhm <wbhmoore@gmail.com>
2018-08-21 15:31:11 -0500

    converted imageOverlay to 'dumb' component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,19 @@ class App extends Component {
   render() {
       const image = "assets/images/ArchesNationalPark.jpg";
       const list = data;
-      const style = {
-          margin: '25px',
-          borderRadius: '10px',
-          fontFamily:'Comic Sans MS'
+      const styles = {
+          button:{
+              margin: '25px',
+              borderRadius: '1px',
+              fontFamily:'Helvetica',
+              fontSize:'16px',
+              letterSpacing: '1px',
+          },
+          listItem:{
+              margin: '3px',
+              fontFamily:'Helvetica',
+              fontSize:'14px',
+          },
       };
     return (
       <div className="App">
@@ -22,11 +31,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
           <ImageOverlay image={image} title="This is a Sample Title" subtitle="This is the slug for the sub title placeholder..." />
-          <Button style={style} variant="contained" color="primary">Sample Button</Button>
+          <Button style={styles.button} variant="contained" color="primary">Sample Button</Button>
           <div>
               {
                   list.map((item)=>{
-                      return <div key={item.id}>{item.name}</div>
+                      return <div style={styles.listItem} key={item.id}>{item.name}</div>
                   })
               }
           </div>
